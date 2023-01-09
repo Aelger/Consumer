8c89165fb72e7f998380c5b799e4c492424866ad
Jalegre <jose.alegre@gmresearch.io>
2023-01-09 09:03:44 -0300

    version para correr en container

--- consumer.py
+++ consumer.py
@@ -1,19 +1,25 @@
 from kafka import KafkaConsumer
 import logging
+import pprint
 import os
 
-log = logging.getLogger("my-logger")
+log = logging.getLogger("CONSUMER-LOG")
 logging.basicConfig(level=logging.INFO)
+
 
 topic = os.environ.get("TOPIC")
 bootstrap_server = os.environ.get("BOOTSTRAP_SERVER")
 
 
-consumer = KafkaConsumer(topic,bootstrap_servers=bootstrap_server)
-log.info("TOPIC SUSCRIBED", consumer.topics())
+consumer = KafkaConsumer(topic, bootstrap_servers=bootstrap_server)
+
+
+log.info("#################### TOPICS ####################")
+log.info(consumer.topics())
 log.info("#################### CONFIGS ####################")
-log.info(consumer.config)
+log.info(pprint.pformat(consumer.config))
 log.info("#################### END ####################")
+
 
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
