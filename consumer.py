1d838aa338d78b411081ed4ae64f63a366dca7ea
Jose <alegrejoseanibal@gmail.com>
2023-02-02 20:30:44 -0300

    test

--- consumer.py
+++ consumer.py
@@ -1,30 +1,43 @@
-from kafka import KafkaConsumer
-import logging
-import pprint
-import json
+# consumer = KafkaConsumer("oracle-jdbc-M_ELASTICPLZ", bootstrap_servers="localhost:9092")
 
-log = logging.getLogger("CONSUMER-LOG")
-logging.basicConfig(
-    format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
-    datefmt='%Y-%m-%d %H:%M:%S')
+from confluent_kafka import Consumer, KafkaError
 
 
-# topic = os.environ.get("TOPIC")
-# bootstrap_server = os.environ.get("BOOTSTRAP_SERVER")
+settings = {
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'my_group',
+    'auto.offset.reset': 'earliest'
+}
 
+def on_assign(consumer, partitions):
+    for partition in partitions:
+        partition.offset = 0
 
-consumer = KafkaConsumer("oracle-jdbc-M_ELASTICPLZ", bootstrap_servers="localhost:9092")
+def on_message(message):
+    if message.error() is not None:
+        if message.error().code() == KafkaError._PARTITION_EOF:
+            print("Reached end of partition.")
+        else:
+            print(f"Error while retrieving message: {message.error()}")
+    else:
+        print(f"Key: {message.key()}, Value: {message.value().decode('utf-8')}")
 
+consumer = Consumer(settings)
+consumer.subscribe(['oracle-jdbc-M_ELASTICPLZ'])
+consumer.assign([])
+consumer.poll(0)
 
-log.info("#################### TOPICS ####################")
-log.info(consumer.topics())
-log.info("#################### CONFIGS ####################")
-log.info(pprint.pformat(consumer.config))
-log.info("#################### END ####################")
+# consumer.seek_to_beginning()
+consumer.assign([p for p in consumer.assignment()])
 
-
-for message in consumer:
-    # message value and key are raw bytes -- decode if necessary!
-    # e.g., for unicode: `message.value.decode('utf-8')`
-    log.info(message.value.decode('ascii'))
+while True:
+    msg = consumer.poll(1.0)
+    if msg is None:
+        continue
+    if msg.error():
+        if msg.error().code() == KafkaError.PARTITION_EOF:
+            print("Reached end of partition.")
+        else:
+            print(f"Error while retrieving message: {msg.error()}")
+    else:
+        on_message(msg)
