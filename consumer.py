2724952809686f77d49131a79c0a1aeaf0159be2
Jose <alegrejoseanibal@gmail.com>
2023-02-01 21:34:22 -0300

    decode msj value to unicode to master

--- consumer.py
+++ consumer.py
@@ -1,17 +1,27 @@
 from kafka import KafkaConsumer
+import logging
+import pprint
 import os
+
+log = logging.getLogger("CONSUMER-LOG")
+logging.basicConfig(level=logging.INFO)
 
 
 topic = os.environ.get("TOPIC")
 bootstrap_server = os.environ.get("BOOTSTRAP_SERVER")
 
 
-consumer = KafkaConsumer(topic, 
-    bootstrap_server, 
-    auto_offset_reset='earliest', 
-    enable_auto_commit=True, 
-    group_id='my-group')
+consumer = KafkaConsumer(topic, bootstrap_servers=bootstrap_server)
 
 
-for msg in consumer:
-    print (msg)
+log.info("#################### TOPICS ####################")
+log.info(consumer.topics())
+log.info("#################### CONFIGS ####################")
+log.info(pprint.pformat(consumer.config))
+log.info("#################### END ####################")
+
+
+for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    log.info(message.value.decode('utf-8'))
