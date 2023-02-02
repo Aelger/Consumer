1be6fde38457002f42a1aa88ca4ae8e417af64b3
Jose <alegrejoseanibal@gmail.com>
2023-02-02 20:32:58 -0300

    test

--- consumer.py
+++ consumer.py
@@ -1,7 +1,13 @@
 # consumer = KafkaConsumer("oracle-jdbc-M_ELASTICPLZ", bootstrap_servers="localhost:9092")
 
 from confluent_kafka import Consumer, KafkaError
+import logging
 
+log = logging.getLogger("CONSUMER-LOG")
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
 
 settings = {
     'bootstrap.servers': 'localhost:9092',
@@ -16,11 +22,11 @@ def on_assign(consumer, partitions):
 def on_message(message):
     if message.error() is not None:
         if message.error().code() == KafkaError._PARTITION_EOF:
-            print("Reached end of partition.")
+            log.info("Reached end of partition.")
         else:
-            print(f"Error while retrieving message: {message.error()}")
+            log.info(f"Error while retrieving message: {message.error()}")
     else:
-        print(f"Key: {message.key()}, Value: {message.value().decode('utf-8')}")
+        log.info(f"Key: {message.key()}, Value: {message.value().decode('utf-8')}")
 
 consumer = Consumer(settings)
 consumer.subscribe(['oracle-jdbc-M_ELASTICPLZ'])
@@ -36,8 +42,8 @@ while True:
         continue
     if msg.error():
         if msg.error().code() == KafkaError.PARTITION_EOF:
-            print("Reached end of partition.")
+            log.info("Reached end of partition.")
         else:
-            print(f"Error while retrieving message: {msg.error()}")
+            log.info(f"Error while retrieving message: {msg.error()}")
     else:
         on_message(msg)
