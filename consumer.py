a934143ed169695cfb081fa6a6eff61ee48e8746
Aelger <alegrejoseanibal@gmail.com>
2023-02-06 11:21:40 -0300

    nuevo for implementado

--- consumer.py
+++ consumer.py
@@ -2,7 +2,8 @@ from kafka import KafkaConsumer
 import os
 import logging
 import pprint
-import json
+# import json
+import base64
 
 topic = os.environ.get("TOPIC")
 bootstrap_server = os.environ.get("BOOTSTRAP_SERVER")
@@ -25,6 +26,7 @@ log.info("#################### CONFIGS ####################")
 log.info(pprint.pformat(consumer.config))
 log.info("#################### END ####################")
 
+"""
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
@@ -32,3 +34,12 @@ for message in consumer:
     log.info("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
                                           value))
+"""
+
+for message in consumer:
+    # message value and key are raw bytes -- decode if necessary!
+    decoded_value = message.value.decode('utf-8')
+    decoded_payload = base64.b64decode(decoded_value['payload']['EMISECC'])
+    log.info("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          decoded_payload))
