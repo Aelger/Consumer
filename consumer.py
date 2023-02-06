a68d49ce0039a7181465a9e7457ded04df9cfd53
Aelger <alegrejoseanibal@gmail.com>
2023-02-06 09:48:48 -0300

    msj formateado

--- consumer.py
+++ consumer.py
@@ -25,4 +25,8 @@ log.info(pprint.pformat(consumer.config))
 log.info("#################### END ####################")
 
 for message in consumer:
-    print(message.value.decode("utf-8"))
+    # message value and key are raw bytes -- decode if necessary!
+    # e.g., for unicode: `message.value.decode('utf-8')`
+    log.info("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
+                                          message.offset, message.key,
+                                          message.value))
