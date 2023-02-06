fd6bcc35381605217f9a7dc94944378d0a232ac7
Aelger <alegrejoseanibal@gmail.com>
2023-02-06 10:25:20 -0300

    add decode json value

--- consumer.py
+++ consumer.py
@@ -2,6 +2,7 @@ from kafka import KafkaConsumer
 import os
 import logging
 import pprint
+import json
 
 topic = os.environ.get("TOPIC")
 bootstrap_server = os.environ.get("BOOTSTRAP_SERVER")
@@ -27,6 +28,7 @@ log.info("#################### END ####################")
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
+    value = json.loads(message.value.decode("utf-8"))
     log.info("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
-                                          message.value.decode('utf-8')))
+                                          value))
