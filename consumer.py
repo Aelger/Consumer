ca4b479c190d4e9736209aa95090c41e85bfb211
Aelger <alegrejoseanibal@gmail.com>
2023-02-06 08:32:42 -0300

    try base consumer

--- consumer.py
+++ consumer.py
@@ -1,11 +1,29 @@
 from kafka import KafkaConsumer
+import os
+import logging
+import pprint
+
+topic = os.environ.get("TOPIC")
+bootstrap_server = os.environ.get("BOOTSTRAP_SERVER")
+
+log = logging.getLogger("CONSUMER-LOG")
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    level=logging.INFO,
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 consumer = KafkaConsumer(
     topic,
-    bootstrap_servers=['localhost:9092'],
+    bootstrap_servers=bootstrap_server,
     group_id='my-group',
-    auto_offset_reset='earliest'
 )
+
+log.info("#################### TOPICS ####################")
+log.info(consumer.topics())
+log.info("#################### CONFIGS ####################")
+log.info(pprint.pformat(consumer.config))
+log.info("#################### END ####################")
 
 for message in consumer:
     print(message.value.decode())
